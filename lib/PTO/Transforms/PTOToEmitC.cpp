0af11f61ea44b8f89f6bb78e98b7a7e5ae4793a0
TaoTao-real <TaoTao-real@users.noreply.github.com>
2026-04-14 09:56:45 +0800

    Adapt TPipe emission to upstream arg order

--- lib/PTO/Transforms/PTOToEmitC.cpp
+++ lib/PTO/Transforms/PTOToEmitC.cpp
@@ -372,14 +372,12 @@ getTPipeDirectionToken(bool isL2G2L, int8_t dirMask, PTOArch targetArch) {
 
 static std::string buildTPipeToken(int32_t flagBase, llvm::StringRef dirTok,
                                    int32_t slotSize, int32_t slotNum,
-                                   bool nosplit,
-                                   std::optional<int32_t> localSlotNum) {
+                                   int32_t localSlotNum, bool nosplit) {
   std::string token = "TPipe<" + std::to_string(flagBase) + ", " + dirTok.str() +
                       ", " + std::to_string(slotSize) + ", " +
                       std::to_string(slotNum);
+  token += ", " + std::to_string(localSlotNum);
   token += nosplit ? ", true" : ", false";
-  if (localSlotNum)
-    token += ", " + std::to_string(*localSlotNum);
   token += ">";
   return token;
 }
@@ -398,9 +396,9 @@ static FailureOr<std::string> buildTPipeTokenFromInitOp(Operation *op,
                                : initOp.getSlotNum();
     return buildTPipeToken(initOp.getFlagBaseAttr().getInt(), *dirTok,
                            initOp.getSlotSize(), initOp.getSlotNum(),
+                           localSlotNum,
                            initOp.getNosplitAttr() &&
-                               initOp.getNosplitAttr().getValue(),
-                           localSlotNum);
+                               initOp.getNosplitAttr().getValue());
   }
 
   if (auto initOp = dyn_cast<pto::InitializeL2LPipeOp>(op)) {
@@ -411,10 +409,9 @@ static FailureOr<std::string> buildTPipeTokenFromInitOp(Operation *op,
     if (failed(dirTok))
       return failure();
     return buildTPipeToken(initOp.getFlagBaseAttr().getInt(), *dirTok,
-                           initOp.getSlotSize(), initOp.getSlotNum(),
+                           initOp.getSlotSize(), initOp.getSlotNum(), 2,
                            initOp.getNosplitAttr() &&
-                               initOp.getNosplitAttr().getValue(),
-                           std::nullopt);
+                               initOp.getNosplitAttr().getValue());
   }
 
   return failure();
